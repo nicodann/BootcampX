883e37939e974d8fa214e1813dfd46f4a77a8b91
Nico Dann <nicodann@gmail.com>
2022-01-17 01:46:54 +0000

    Add join query

---
+++ 1_queries/rollover_students.sql
@@ -0,0 +1,4 @@
+SELECT students.name, students.start_date as student_start_date, cohorts.name, cohorts.start_date as cohort_start_date
+FROM students JOIN cohorts ON cohort_id = cohorts.id 
+WHERE students.start_date != cohorts.start_date 
+ORDER BY cohort_start_date;
